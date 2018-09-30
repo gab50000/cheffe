620df8cdc00536d4c4ee8a074371a0a6893db154
gk <gabriel.kabbe@mail.de>
2018-09-30 20:20:18 +0200

    Add preparation info

--- chefkoch/spiders/main.py
+++ chefkoch/spiders/main.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 import unicodedata
 
+import re
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.shell import inspect_response
 from scrapy.utils.response import open_in_browser
+
+
+def cleanup(string):
+    return re.sub(r"\s{2,}", "", string)
 
 
 class MainSpider(CrawlSpider):
@@ -29,6 +34,10 @@ class MainSpider(CrawlSpider):
         i['ingredients'] = ingredients
 
         i['title'] = response.xpath(r"//h1[@class='page-title']/text()").extract_first()
+        infos = response.xpath(r"//p[@id='preparation-info']/descendant-or-self::*/text()").extract()
+        infos = (cleanup(i) for i in infos)
+        infos = [i for i in infos if i]
+        i['infos'] = list(zip(infos[::2], infos[1::2]))
 
         votes = response.xpath(r"//span[contains(@class, 'total-votes')]/text()")
         if votes:
